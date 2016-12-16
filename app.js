388d3b2fea4e779d7babc8553630531a3301dc61
Kyle McDonald <kyle@kylemcdonald.net>
2016-12-15 17:14:43 -0800

    search, manual add, recent, and all working

--- app.js
+++ app.js
@@ -1,9 +1,83 @@
+// load all requirements and create express app
 var _ = require('lodash');
 var fs = require('fs');
 var http = require('http');
+var requestIp = require('request-ip');
 var express = require('express');
+var touch = require('touch');
 var app = express();
 
+// load whitelist from env in format:
+// WHITELIST='["::1", "127.0.0.1"]'
+var whitelist = JSON.parse(process.env.WHITELIST);
+console.log('Loaded whitelist:');
+whitelist.forEach(function (ip) {
+	console.log(" - " + ip);
+})
+
+// load database of captioned content from DB_FILE
+console.log('Loading from database ' + process.env.DB_FILE);
+var precaption = [];
+var postcaption = [];
+touch(process.env.DB_FILE); // make sure it exists
+var rawdb = fs.readFileSync(process.env.DB_FILE, 'utf8');
+rawdb.split(/\r?\n/).forEach((line) => {
+	line = line.replace(/\r?\n/, '');
+	if(line.length > 0) {
+		try {
+			var cur = JSON.parse(line);
+			precaption.push(cur);
+			postcaption.push(cur);
+		} catch (e) {
+			console.log('Error in database: ' + line);
+		}
+	}
+})
+console.log('Loaded ' + postcaption.length + ' records.');
+
+function getDescription(url, cb, err) {
+	var postData = JSON.stringify({
+	 	'url' : url
+	});
+
+	var options = {
+		hostname: 'api.projectoxford.ai',
+		port: 80,
+		path: '/vision/v1.0/describe',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Ocp-Apim-Subscription-Key': process.env.OCP_API_KEY
+		}
+	};
+
+	var req = http.request(options, (res) => {
+		if(res.statusCode == 200) {
+			res.setEncoding('utf8');
+			res.on('data', (chunk) => {
+				var data = JSON.parse(chunk);
+				console.log(data);
+				var caption = data.description.captions[0].text;
+				if(typeof caption !== "undefined") {
+					cb(caption);
+				} else {
+					console.log("Caption is undefined.");
+				}
+			});
+		} else {
+			err(res.statusCode);
+		}
+	});
+
+	req.on('error', (e) => {
+		err(e.message);
+	});
+
+	req.write(postData);
+	req.end();
+}
+
+// create instagram private api connection
 var Promise = require('bluebird');
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device(process.env.USERNAME);
@@ -14,6 +88,16 @@ Client.Session.create(device, storage, process.env.USERNAME, process.env.PASSWOR
 		session = s;
 		console.log('Logged in to Instagram with username ' + process.env.USERNAME);
 	})
+
+// routes
+app.use(requestIp.mw());
+app.get('/', (req, res, next) => {
+	if(whitelist.indexOf(req.clientIp) != -1) {
+		next();
+	} else {
+		res.status(403).end('Forbidden: ' + req.clientIp);
+	}
+});
 
 app.use('/', express.static('public'));
 
@@ -26,7 +110,7 @@ app.use(function (req, res, next) {
 	}
 })
 
-app.get('/latest', function (req, res) {
+app.get('/search', function (req, res) {
 	var query = req.query;
 	if(typeof(query.tag) === undefined) {
 		res.sendStatus(500);
@@ -54,6 +138,77 @@ app.get('/latest', function (req, res) {
 		});
 		res.send(compiled);
 	});
+});
+
+app.get('/all.json', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.json(postcaption);
+});
+
+app.get('/recent.json', (req, res) => {
+	var limit = req.query.limit || 10;
+
+	res.setHeader('Content-Type', 'application/json');
+	if(postcaption.length < 1) {
+		res.json({});
+		return;
+	}
+	var sorted = _.sortBy(postcaption, 'timestamp');
+	var last = sorted.slice(-limit); // send 10 most recent
+	res.json(last);
+});
+
+function add(img) {
+	if(typeof(img.url) === 'undefined') {
+		console.error('No url for image:');
+		console.error(img);
+		return;
+	}
+	if(typeof(img.timestamp) === 'undefined') {
+		console.error('No timestamp for image:');
+		console.error(img);
+		return;
+	}
+
+	// need to check if the url is already present
+	var dup;
+	if(dup = _.findLast(precaption, {'url': img.url})) {
+		var timeSinceDup = img.timestamp - dup.timestamp;
+		if(timeSinceDup < process.env.MAX_TIME_SINCE_DUP) {
+			console.log('Ignoring duplicate ' + timeSinceDup + 'ms ago.');
+		} else {
+			console.log('Using caption from duplicate ' + timeSinceDup + 'ms ago.');	
+			img.text = dup.text;
+			// add duplicate to the current state, but don't save to the log
+			postcaption.push(img);
+		}
+		return;
+	}
+	precaption.push(img);
+	
+	getDescription(img.url, (description) => {
+		img.text = description;
+		postcaption.push(img);
+		fs.appendFileSync(process.env.DB_FILE, JSON.stringify(img) + '\n');
+		console.log(img);
+	}, (err) => {
+		console.log('Error: ' + err);
+	});
+}
+
+app.get('/add', (req, res) => {
+	res.end();
+
+	var i = req.url.indexOf('?');
+	var url = req.url.substr(i + 1);
+
+	var img = {
+		timestamp: new Date().getTime(),
+		url: url
+	};
+	console.log(img);
+
+	add(img);
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
